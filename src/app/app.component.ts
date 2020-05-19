b2ba636cc9f508a1b6de8fe2220d450acfa9281a
Noah Sylwester <nanoabram@comcast.net>
2020-05-18 18:20:28 -0700

    initial commit. add angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'Angular Test';
+  myArray = [1,2,3];
+  inputValue = '';
+  onChange(item) {
+    this.inputValue = item;
+  }
+  increment() {
+    this.myArray.push(this.myArray.length + 1);
+  }
 }
